0daaaa9923e17af3deec5d970fcd000e7b5f49ce
tienbot1 <tien.ru@mail.ru>
2023-03-14 23:18:36 +0900

    adaptive product cards

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-let screenWidth = window.screen.width;
 let test = document.querySelectorAll('.card__price');
+let pizza = document.querySelector('#pizza');
 
-if (screenWidth <= 576){
-    for(i = 0; i < test.length; i++) {
-        test[i].classList.toggle("button");
+
+
+addEventListener("resize", (event) => {
+    let screenWidth = window.screen.width;
+    if (screenWidth <= 576){
+        for(i = 0; i < test.length; i++) {
+            test[i].classList.add("button");
+        }
+        pizza.querySelector('.card__price').classList.add("button--orange");
+    } else {
+        for(i = 0; i < test.length; i++) {
+            test[i].classList.remove("button");
+        }
+        pizza.querySelector('.card__price').classList.remove("button--orange");
     }
-}
+});
