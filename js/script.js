28534a192f1933f4d48ba37b4b59506ccebecf70
tienbot1 <tien.ru@mail.ru>
2023-03-16 20:48:41 +0900

    get modal / close modal

--- js/script.js
+++ js/script.js
@@ -1,19 +1,35 @@
-let test = document.querySelectorAll('.card__price');
+let html = document.getElementsByTagName('html');
+let price = document.querySelectorAll('.card__price');
 let pizza = document.querySelector('#pizza');
+let buy = document.querySelectorAll('.buy');
+let modal = document.querySelector('.modal__back');
+let close = document.querySelector('.modal__close');
 
 
-
+// адаптив стилей
 addEventListener("resize", (event) => {
     let screenWidth = window.screen.width;
     if (screenWidth <= 576){
-        for(i = 0; i < test.length; i++) {
-            test[i].classList.add("button");
+        for(i = 0; i < price.length; i++) {
+            price[i].classList.add("button");
         }
         pizza.querySelector('.card__price').classList.add("button--orange");
     } else {
-        for(i = 0; i < test.length; i++) {
-            test[i].classList.remove("button");
+        for(i = 0; i < price.length; i++) {
+            price[i].classList.remove("button");
         }
         pizza.querySelector('.card__price').classList.remove("button--orange");
     }
+});
+
+// вызов модалки
+for(i = 0; i < buy.length; i++) {
+    buy[i].addEventListener("click", (event) => {
+        modal.classList.remove("d-none");
+    });
+}
+
+// закрыть модалку
+close.addEventListener("click", (event) => {
+    modal.classList.add("d-none");
 });
